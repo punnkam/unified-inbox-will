513bdc6a8e2cbbfc9cca7afe9acbc6efd5c16252
Will Garman <wgarman702@gmail.com>
2024-06-05 01:33:50 -0400

    fix hover state for selected on settings sidebar

--- app/(protected-paths)/settings/SettingsSideBarOption.tsx
+++ app/(protected-paths)/settings/SettingsSideBarOption.tsx
@@ -15,7 +15,7 @@ export const SettingsSideBarOption = ({
       <p
         className={cn(
           "w-full px-6 py-2 text-subtitle-xs hover:bg-selected-subtle",
-          selected && "bg-selected"
+          selected && "bg-selected hover:bg-selected"
         )}
       >
         {name}
