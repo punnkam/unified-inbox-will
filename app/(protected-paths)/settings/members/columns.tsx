f7ca14b82aa18950d47bec8dfecdf8172dbb512e
Will Garman <wgarman702@gmail.com>
2024-06-06 01:10:31 -0400

    main face of Members page

---
+++ app/(protected-paths)/settings/members/columns.tsx
@@ -0,0 +1,84 @@
+// docs: https://ui.shadcn.com/docs/components/data-table
+
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { MoreHorizontal } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type Member = {
+  id: string;
+  name: string;
+  role: "Admin" | "Member" | "External Team";
+  email: string;
+  image: string;
+  status: "Active" | "Pending";
+};
+
+export const columns: ColumnDef<Member>[] = [
+  {
+    accessorKey: "name",
+    header: "Name",
+    cell: ({ row }) => {
+      return (
+        <div className="flex gap-3 items-center">
+          <img
+            src={row.original.image}
+            alt={row.original.name}
+            className="w-10 h-10 rounded-full object-cover"
+          />
+          <div>
+            <p className="text-subtitle-xs">{row.original.name}</p>
+            <p className="text-tertiary text-body-2xs font-normal">
+              {row.original.email}
+            </p>
+          </div>
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+  },
+  {
+    accessorKey: "role",
+    header: "Role",
+    cell: ({ row }) => {
+      return (
+        <p className="text-tertiary text-body-2xs font-normal">
+          {row.original.role}
+        </p>
+      );
+    },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const member = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild className="self-end">
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4 text-icon-tertiary" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem>Edit Role</DropdownMenuItem>
+            <DropdownMenuItem>Remove Member</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
---
+++ app/(protected-paths)/settings/members/page.tsx
@@ -0,0 +1,72 @@
+import { Button } from "@/components/ui/button";
+import { Member, columns } from "./columns";
+import { DataTable } from "./data-table";
+import { PlusIcon } from "lucide-react";
+
+async function getData(): Promise<Member[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "1",
+      name: "James Doe",
+      role: "Admin",
+      email: "m@example.com",
+      status: "Active",
+      image:
+        "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      id: "2",
+      name: "Jack Doe",
+      role: "Member",
+      email: "jack@gmail.com",
+      status: "Active",
+      image:
+        "https://images.unsplash.com/photo-1580489944761-15a19d654956?q=80&w=1961&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      id: "3",
+      name: "John Doe",
+      role: "External Team",
+      email: "john@gmail.com",
+      status: "Active",
+      image:
+        "https://plus.unsplash.com/premium_photo-1664536392896-cd1743f9c02c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      id: "4",
+      name: "Jose Doe",
+      role: "Member",
+      email: "jose@gmail.com",
+      status: "Pending",
+      image:
+        "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    // ...
+  ];
+}
+
+export default async function MembersPage() {
+  const data = await getData();
+
+  return (
+    <div className="flex flex-col gap-[28px]">
+      <div className="flex justify-between items-center">
+        <div className="flex flex-col gap-3">
+          <h1 className="text-title-2xl">Members</h1>
+          <p className="text-subtitle-sm text-tertiary">
+            Add and manage your members
+          </p>
+        </div>
+        <Button variant="default">
+          <PlusIcon className="h-5 w-5 mr-2" />
+          Add
+        </Button>
+      </div>
+
+      <div className="border-b stroke-primary"></div>
+
+      <DataTable columns={columns} data={data} />
+    </div>
+  );
+}
