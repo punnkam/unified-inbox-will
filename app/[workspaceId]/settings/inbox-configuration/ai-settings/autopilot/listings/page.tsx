691243252aa9e4bb392ce61b333f0a8f337fc7af
Will Garman <wgarman702@gmail.com>
2024-06-15 00:34:43 -0400

    Added all of AI listings table

---
+++ app/[workspaceId]/settings/inbox-configuration/ai-settings/autopilot/listings/page.tsx
@@ -0,0 +1,61 @@
+import { Button } from "@/components/ui/button";
+import { columns } from "./columns";
+import { DataTable } from "./data-table";
+import Link from "next/link";
+import { ListingWithDeleteHandler } from "@/lib/types";
+import { fetchListings } from "@/app/actions";
+import { ArrowNarrowLeft } from "@/components/icons/CustomIcons";
+
+async function getData(
+  workspaceId: string
+): Promise<ListingWithDeleteHandler[]> {
+  // Get the current workspace member data
+  const data = await fetchListings(workspaceId);
+
+  if (!data) {
+    return [];
+  }
+
+  return data;
+}
+
+export default async function AiListingsPage({
+  params: { workspaceId },
+}: {
+  params: { workspaceId: string };
+}) {
+  const data = await getData(workspaceId);
+
+  const groups: number[] = [];
+
+  // get all unique groups
+  data.map((listing) => {
+    if (listing.group && !groups.includes(listing.group!)) {
+      groups.push(listing.group!);
+    }
+  });
+
+  // sort so lowest group is first
+  groups.sort((a, b) => a - b);
+
+  return (
+    <div className="flex flex-col gap-[28px]">
+      <div className="flex flex-col gap-4">
+        <Link href="../ai-settings">
+          <Button
+            variant="link"
+            className="text-tertiary flex items-center gap-2 p-2"
+          >
+            <ArrowNarrowLeft />
+            AI settings
+          </Button>
+        </Link>
+        <h1 className="text-title-2xl">Listings on autopilot</h1>
+      </div>
+
+      <div className="border-b border-primary"></div>
+
+      <DataTable columns={columns} data={data} groups={groups} />
+    </div>
+  );
+}
--- lib/types.tsx
+++ lib/types.tsx
@@ -132,6 +132,8 @@ export type Listing = {
   title: string;
   address: string;
   image: string;
+  autopilot: boolean;
+  group?: number;
 };
 
 export type MemberWithTeamId = Member & { teamId: number };
@@ -499,6 +501,8 @@ export const fakeListingsData: Listing[] = [
     address: "1234 Elm Street",
     image:
       "https://images.unsplash.com/photo-1518780664697-55e3ad937233?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    autopilot: true,
+    group: 3,
   },
   {
     id: 2,
@@ -508,6 +512,7 @@ export const fakeListingsData: Listing[] = [
     address: "5678 Oak Street",
     image:
       "https://images.unsplash.com/photo-1518780664697-55e3ad937233?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    autopilot: false,
   },
   {
     id: 3,
@@ -517,6 +522,41 @@ export const fakeListingsData: Listing[] = [
     address: "91011 Pine Street",
     image:
       "https://images.unsplash.com/photo-1518780664697-55e3ad937233?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    autopilot: true,
+    group: 2,
+  },
+  {
+    id: 4,
+    workspaceId: 1,
+    active: false,
+    title: "Spacious 3 Bedroom House",
+    address: "91011 Pine Street",
+    image:
+      "https://images.unsplash.com/photo-1518780664697-55e3ad937233?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    autopilot: true,
+    group: 1,
+  },
+  {
+    id: 5,
+    workspaceId: 1,
+    active: false,
+    title: "Spacious 3 Bedroom House",
+    address: "91011 Pine Street",
+    image:
+      "https://images.unsplash.com/photo-1518780664697-55e3ad937233?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    autopilot: false,
+    group: 2,
+  },
+  {
+    id: 6,
+    workspaceId: 1,
+    active: false,
+    title: "Spacious 3 Bedroom House",
+    address: "91011 Pine Street",
+    image:
+      "https://images.unsplash.com/photo-1518780664697-55e3ad937233?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    autopilot: true,
+    group: 3,
   },
 ];
 
