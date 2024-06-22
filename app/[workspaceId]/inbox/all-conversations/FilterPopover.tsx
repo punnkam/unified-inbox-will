7b0d1605ca06cd6d0589c28b8d0d7ae76e340609
Will Garman <wgarman702@gmail.com>
2024-06-22 14:48:47 -0400

    added pinned filters

--- app/[workspaceId]/inbox/all-conversations/FilterPopover.tsx
+++ app/[workspaceId]/inbox/all-conversations/FilterPopover.tsx
@@ -18,11 +18,11 @@ import {
   DropdownMenuInboxContent,
 } from "@/components/ui/dropdown-menu";
 import { ColumnFiltersState } from "@tanstack/react-table";
-import { FilterLinesIcon } from "@/components/icons/CustomIcons";
+import { FilterLinesIcon, Pin02Icon } from "@/components/icons/CustomIcons";
 import { XIcon } from "lucide-react";
 import { allFilters, AllFilters, FilterValue } from "@/lib/types";
 import { Checkbox } from "@/components/ui/checkbox";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { IconComponent } from "@/components/icons/IconComponent";
 
 type FilterValues = { [key: string]: FilterValue[] };
@@ -37,6 +37,31 @@ export const FilterPopover = ({
   clearFilters: () => void;
 }) => {
   const [open, setOpen] = useState(false);
+  const [pinned, setPinned] = useState<FilterValues>({});
+  const [filterTitles, setFilterTitles] = useState<{ [key: string]: string }>(
+    {}
+  );
+
+  // Find/Set the pinned filters on mount - to avoid infinate rerenders
+  useEffect(() => {
+    const pinnedFilters: FilterValues = {};
+    const titles: { [key: string]: string } = {};
+
+    Object.entries(allFilters).forEach(([key, filter]) => {
+      titles[key] = filter.title;
+      filter.options.forEach((option) => {
+        if (typeof option === "object" && option.pinned) {
+          if (!pinnedFilters[key]) {
+            pinnedFilters[key] = [];
+          }
+          pinnedFilters[key].push(option);
+        }
+      });
+    });
+
+    setPinned(pinnedFilters);
+    setFilterTitles(titles);
+  }, []);
 
   const handleSelect = (
     filter: keyof AllFilters,
@@ -64,6 +89,27 @@ export const FilterPopover = ({
     setColumnFilters(columnId, { ...currentFilter, ...newFilter });
   };
 
+  // updates the local state for pinned filters - you would also want to call a backend here to save the pin
+  const handlePinToggle = (filterId: string, label: FilterValue) => {
+    setPinned((prevPinned) => {
+      const updatedPinned = { ...prevPinned };
+      if (!updatedPinned[filterId]) {
+        updatedPinned[filterId] = [];
+      }
+      const isPinned = updatedPinned[filterId].some(
+        (item) => item.id === label.id
+      );
+      if (isPinned) {
+        updatedPinned[filterId] = updatedPinned[filterId].filter(
+          (item) => item.id !== label.id
+        );
+      } else {
+        updatedPinned[filterId].push(label);
+      }
+      return updatedPinned;
+    });
+  };
+
   return (
     <div>
       <DropdownMenu open={open} onOpenChange={() => setOpen(!open)}>
@@ -85,6 +131,99 @@ export const FilterPopover = ({
             />
           </div>
           <div className="p-2">
+            <DropdownMenuSub key="pinned">
+              <DropdownMenuSubTrigger className="p-2 hover:bg-hover rounded-md cursor-pointer">
+                <div className="flex items-center justify-between w-full">
+                  <div className="flex items-center gap-2">
+                    <span className="size-6 flex items-center justify-center">
+                      <Pin02Icon className="text-icon-tertiary size-[15px]" />
+                    </span>
+                    <p className="text-subtitle-xs">Pinned filters</p>
+                  </div>
+                </div>
+              </DropdownMenuSubTrigger>
+              <DropdownMenuSubContent className="p-0">
+                <Command>
+                  <CommandInput
+                    placeholder={`Search pinned`}
+                    autoFocus={true}
+                  />
+                  <CommandList>
+                    <CommandEmpty>No pinned filters.</CommandEmpty>
+                    <CommandGroup className="py-0">
+                      {pinned &&
+                        Object.entries(pinned).map(
+                          ([pinnedCategory, filters]) =>
+                            filters.length > 0 && (
+                              <div
+                                key={pinnedCategory}
+                                className="flex flex-col gap-1 pt-2 pb-[10px]"
+                              >
+                                <p className="text-body-3xs text-tertiary px-2">
+                                  {filterTitles[pinnedCategory]}
+                                </p>
+                                {filters.map((filter) => {
+                                  // Get the column Id
+                                  const columnId =
+                                    allFilters[
+                                      pinnedCategory as keyof AllFilters
+                                    ]!.column;
+
+                                  const currentFilter = columnFilters.find(
+                                    (appliedFilter) =>
+                                      appliedFilter.id === columnId
+                                  )?.value as FilterValues;
+
+                                  // Check if the filter is already applied
+                                  const isChecked = currentFilter?.[
+                                    pinnedCategory as keyof FilterValues
+                                  ]?.some((item) => item.id === filter.id);
+
+                                  return (
+                                    <CommandItem
+                                      key={filter.id!}
+                                      value={filter.name}
+                                      onSelect={() =>
+                                        handleSelect(
+                                          pinnedCategory as keyof AllFilters,
+                                          filter,
+                                          columnId
+                                        )
+                                      }
+                                      className="flex items-center justify-between"
+                                    >
+                                      <div className="flex items-center gap-2 text-subtitle-sm hover:cursor-pointer">
+                                        <Checkbox checked={isChecked} />
+                                        {filter.image && (
+                                          <img
+                                            src={filter.image}
+                                            className="w-6 h-6 rounded-full object-cover"
+                                          />
+                                        )}
+                                        {filter.name}
+                                      </div>
+                                      <Pin02Icon
+                                        className="text-icon-tertiary size-[15px] hover:cursor-pointer fill-icon-tertiary"
+                                        onClick={(e) => {
+                                          e.stopPropagation();
+                                          handlePinToggle(
+                                            pinnedCategory,
+                                            filter
+                                          );
+                                        }}
+                                      />
+                                    </CommandItem>
+                                  );
+                                })}
+                              </div>
+                            )
+                        )}
+                    </CommandGroup>
+                  </CommandList>
+                </Command>
+              </DropdownMenuSubContent>
+            </DropdownMenuSub>
+
             {Object.keys(allFilters).map((filter) => {
               const filterKey = filter as keyof AllFilters;
               const title = allFilters[filterKey]!.title;
@@ -124,21 +263,20 @@ export const FilterPopover = ({
                         autoFocus={true}
                       />
                       <CommandList>
-                        <CommandEmpty>No label found.</CommandEmpty>
+                        <CommandEmpty>No filter found.</CommandEmpty>
                         <CommandGroup>
                           {filterOptions.map((label) => {
-                            // check if label & data is an object (ugly typscript)
-
-                            const checked = (
-                              currentFilter?.[filterKey] as FilterValue[]
-                            )?.some((item: FilterValue) => {
+                            const isChecked = (item: FilterValue) => {
                               if (
-                                typeof item !== "string" &&
-                                typeof item !== "number"
+                                typeof item === "object" &&
+                                typeof label === "object"
                               ) {
                                 return item.id === label.id;
                               }
-                            });
+                              return item === label;
+                            };
+                            const checked =
+                              currentFilter?.[filterKey]?.some(isChecked);
 
                             return (
                               <CommandItem
@@ -147,14 +285,31 @@ export const FilterPopover = ({
                                 onSelect={() =>
                                   handleSelect(filterKey, label, columnId)
                                 }
-                                className="flex items-center gap-2 text-subtitle-sm hover:cursor-pointer"
+                                className="flex items-center justify-between group"
                               >
-                                <Checkbox checked={checked} />
-                                <img
-                                  src={label.image}
-                                  className="w-6 h-6 rounded-full object-cover"
+                                <div className="flex items-center gap-2 text-subtitle-sm hover:cursor-pointer">
+                                  <Checkbox checked={checked} />
+                                  {label.image && (
+                                    <img
+                                      src={label.image}
+                                      className="w-6 h-6 rounded-full object-cover"
+                                    />
+                                  )}
+                                  {label.name}
+                                </div>
+                                <Pin02Icon
+                                  className={`${
+                                    pinned[filterKey]?.some(
+                                      (item) => item.id === label.id
+                                    )
+                                      ? "fill-icon-tertiary"
+                                      : "hidden group-hover:block"
+                                  } text-icon-tertiary size-[15px] hover:cursor-pointer`}
+                                  onClick={(e) => {
+                                    e.stopPropagation();
+                                    handlePinToggle(filterKey, label);
+                                  }}
                                 />
-                                {label.name}
                               </CommandItem>
                             );
                           })}
