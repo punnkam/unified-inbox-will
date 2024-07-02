182c28f9b217c933f0c47679884176ef62b899bc
Will Garman <wgarman702@gmail.com>
2024-07-01 21:47:51 -0400

    scroll area color to match main scrollbar

--- components/ui/scroll-area.tsx
+++ components/ui/scroll-area.tsx
@@ -33,14 +33,14 @@ const ScrollBar = React.forwardRef<
     className={cn(
       "flex touch-none select-none transition-colors",
       orientation === "vertical" &&
-        "h-full w-2.5 border-l border-l-transparent p-[1px]",
+        "h-full w-2 border-l border-l-transparent p-[1px]",
       orientation === "horizontal" &&
         "h-2 flex-col border-t border-t-transparent p-[1px]",
       className
     )}
     {...props}
   >
-    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-pressed hover:cursor-grab active:cursor-grabbing" />
+    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-[#71717a] hover:cursor-grab active:cursor-grabbing" />
   </ScrollAreaPrimitive.ScrollAreaScrollbar>
 ));
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName;
