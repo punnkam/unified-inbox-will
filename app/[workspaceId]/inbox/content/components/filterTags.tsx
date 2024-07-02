b504df1e5d812930e18df99744601b513145ff76
Will Garman <wgarman702@gmail.com>
2024-07-01 20:01:14 -0400

    wait no this is the filter count, other one was the filter button

--- app/[workspaceId]/inbox/content/components/filterTags.tsx
+++ app/[workspaceId]/inbox/content/components/filterTags.tsx
@@ -5,7 +5,6 @@ import { XIcon } from "lucide-react";
 import { IconComponent } from "@/components/icons/IconComponent";
 import { ConversationTable } from "@/lib/realDataSchema";
 import { Dispatch, SetStateAction } from "react";
-import { set } from "date-fns";
 import { useTableContext } from "../../TableContext";
 
 export const Tag = ({
@@ -38,6 +37,7 @@ export const FilterTags = ({
   clearFilters,
   removeFilter,
   table,
+  view,
 }: {
   clearFilters: (
     table: ConversationTable,
@@ -49,6 +49,7 @@ export const FilterTags = ({
     setColumnFilters: Dispatch<SetStateAction<ColumnFiltersState>>
   ) => void;
   table: ConversationTable;
+  view?: "landing" | "chat";
 }) => {
   const { columnFilters, setColumnFilters } = useTableContext();
 
@@ -61,53 +62,74 @@ export const FilterTags = ({
       Object.values(filter.value).some((filterValue) => filterValue.length > 0)
   );
 
+  const filterCount = columnFilters.reduce((count, filter) => {
+    if (typeof filter.value === "object" && filter.value !== null) {
+      return (
+        count +
+        Object.values(filter.value).reduce(
+          (subCount, filterValue) => subCount + filterValue.length,
+          0
+        )
+      );
+    }
+    return count;
+  }, 0);
+
   return (
-    hasTags && (
-      <div className={`flex gap-2 flex-wrap px-9 pt-4 pb-2`}>
-        {columnFilters.map((filter) => {
-          if (
-            typeof filter.value === "object" &&
-            filter.value !== null &&
-            Object.keys(filter.value).length > 0
-          ) {
-            return Object.entries(
-              filter.value as { [key in keyof AllFilters]: FilterValue[] }
-            ).map(([key, values]) => {
-              if (values.length > 0) {
-                const displayValue =
-                  values.length > 2
-                    ? `${values.length} selected`
-                    : values
-                        .map((value) =>
-                          typeof value === "object" ? value.name : value
-                        )
-                        .join(", ");
-                return (
-                  <Tag
-                    key={filter.id}
-                    title={allFilters[key as keyof AllFilters]!.title}
-                    icon={allFilters[key as keyof AllFilters]!.icon}
-                    value={displayValue}
-                    onRemove={() =>
-                      removeFilter(filter.id, key, setColumnFilters)
-                    }
-                  />
-                );
+    <>
+      {view === "chat" && filterCount > 0 ? (
+        <div className="absolute top-[1px] right-[2px] flex items-center justify-center size-[14px] border border-white rounded-full bg-icon-active text-primary-inverse text-[8px] font-medium">
+          {filterCount}
+        </div>
+      ) : (
+        hasTags && (
+          <div className={`flex gap-2 flex-wrap px-9 pt-4 pb-2`}>
+            {columnFilters.map((filter) => {
+              if (
+                typeof filter.value === "object" &&
+                filter.value !== null &&
+                Object.keys(filter.value).length > 0
+              ) {
+                return Object.entries(
+                  filter.value as { [key in keyof AllFilters]: FilterValue[] }
+                ).map(([key, values]) => {
+                  if (values.length > 0) {
+                    const displayValue =
+                      values.length > 2
+                        ? `${values.length} selected`
+                        : values
+                            .map((value) =>
+                              typeof value === "object" ? value.name : value
+                            )
+                            .join(", ");
+                    return (
+                      <Tag
+                        key={filter.id}
+                        title={allFilters[key as keyof AllFilters]!.title}
+                        icon={allFilters[key as keyof AllFilters]!.icon}
+                        value={displayValue}
+                        onRemove={() =>
+                          removeFilter(filter.id, key, setColumnFilters)
+                        }
+                      />
+                    );
+                  }
+                  return null;
+                });
               }
               return null;
-            });
-          }
-          return null;
-        })}
+            })}
 
-        <Button
-          variant="ghost"
-          size={"sm"}
-          onClick={() => clearFilters(table, columnFilters)}
-        >
-          <p className="text-secondary text-subtitle-sm">Clear All</p>
-        </Button>
-      </div>
-    )
+            <Button
+              variant="ghost"
+              size={"sm"}
+              onClick={() => clearFilters(table, columnFilters)}
+            >
+              <p className="text-secondary text-subtitle-sm">Clear All</p>
+            </Button>
+          </div>
+        )
+      )}
+    </>
   );
 };
