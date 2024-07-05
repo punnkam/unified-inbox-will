eed8f6e77b8d2d65f77278fa396c00610ec6d017
Will Garman <wgarman702@gmail.com>
2024-07-04 22:52:02 -0500

    removed logs

--- app/[workspaceId]/inbox/chat/page.tsx
+++ app/[workspaceId]/inbox/chat/page.tsx
@@ -10,9 +10,6 @@ export default async function AllConversationsPage({
   params: { workspaceId: string };
   searchParams: { c: string };
 }) {
-  console.log("workspaceId", workspaceId);
-  console.log("conersationId", c);
-
   const data = await fetchConversation(c);
 
   if (!data || !data.success || !data.data) {
