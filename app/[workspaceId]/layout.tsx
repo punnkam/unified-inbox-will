e6ca8170196a3e42bf4a71ec7f4dcd902f7b6316
Will Garman <wgarman702@gmail.com>
2024-07-07 16:24:15 -0400

    add tooltip provider

--- app/[workspaceId]/layout.tsx
+++ app/[workspaceId]/layout.tsx
@@ -3,6 +3,7 @@
 
 import React from "react";
 import { SideBar } from "./SideBar";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
 export default function ProtectedPathsLayout({
   children,
@@ -14,19 +15,21 @@ export default function ProtectedPathsLayout({
   // some sort of auth check here
 
   return (
-    <div className="flex">
-      {/* Side bar */}
-      <SideBar />
+    <TooltipProvider>
+      <div className="flex">
+        {/* Side bar */}
+        <SideBar />
 
-      {/* Page Content */}
-      <div
-        className="flex-grow bg-primary overflow-x-clip"
-        style={{
-          boxShadow: "-10px 4px 15px 0px rgba(0, 0, 0, 0.02)",
-        }}
-      >
-        {children}
+        {/* Page Content */}
+        <div
+          className="flex-grow bg-primary overflow-x-clip"
+          style={{
+            boxShadow: "-10px 4px 15px 0px rgba(0, 0, 0, 0.02)",
+          }}
+        >
+          {children}
+        </div>
       </div>
-    </div>
+    </TooltipProvider>
   );
 }
