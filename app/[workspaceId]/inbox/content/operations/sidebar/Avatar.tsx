01e5e26e26f6584b25e30e0f2c9cc05dd5eb0b47
Will Garman <wgarman702@gmail.com>
2024-07-09 17:24:39 -0400

    avatar component

---
+++ app/[workspaceId]/inbox/content/operations/sidebar/Avatar.tsx
@@ -0,0 +1,49 @@
+import { cn } from "@/lib/utils";
+import { User03Icon } from "@/components/icons/CustomIcons";
+
+export const Avatar = ({
+  size,
+  image,
+  greenDot,
+}: {
+  size: "small" | "medium" | "large";
+  image?: string;
+  greenDot?: boolean;
+}) => {
+  const sizeClass =
+    size === "small"
+      ? "size-6 min-w-6 min-h-6"
+      : size === "medium"
+      ? "size-9 min-w-9 min-h-9"
+      : "size-10 min-w-10 min-h-10";
+
+  return (
+    <div className="relative">
+      {image ? (
+        <img
+          src={image}
+          alt="Guest Image"
+          className={cn("rounded-full object-cover", sizeClass)}
+        />
+      ) : (
+        <div
+          className={cn(
+            "rounded-full bg-secondary flex items-center justify-center",
+            sizeClass
+          )}
+        >
+          <User03Icon className="size-fit text-icon-tertiary" />
+        </div>
+      )}
+      {greenDot && size === "small" && (
+        <div className="absolute bottom-[1px] right-0 size-[6px] rounded-full bg-icon-success"></div>
+      )}
+      {greenDot && size === "medium" && (
+        <div className="absolute bottom-[1px] right-0 size-[9px] rounded-full bg-icon-success"></div>
+      )}
+      {greenDot && size === "large" && (
+        <div className="absolute bottom-[1px] right-0 size-[10px] rounded-full bg-icon-success"></div>
+      )}
+    </div>
+  );
+};
